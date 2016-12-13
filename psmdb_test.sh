757272cfe9ed59dc19cbb8829de187089ca9509a
hrvojem <hmatijak@gmail.com>
2016-12-13 11:36:41 +0100

    Versions update

--- psmdb_test.sh
+++ psmdb_test.sh
@@ -5,11 +5,23 @@ echo -n > /tmp/psmdb_run.log
 
 set -e
 
+# Enable auditLog and profiling/rate limit to see if services start with those
+sed -i 's/#operationProfiling:/operationProfiling:\n  mode: all\n  slowOpThresholdMs: 200\n  rateLimit: 100/' /etc/mongod.conf
+sed -i 's/#auditLog:/audit:\n  destination: file\n  path: \/tmp\/audit.json/' /etc/mongod.conf
+
 function start_service {
-	if [ "$(lsb_release -sc)" = "trusty" ]; then
-        	echo "starting mongod service on trusty..."
+        local redhatrelease=""
+        if [ -f /etc/redhat-release ]; then
+		redhatrelease=$(cat /etc/redhat-release | grep -o '[0-9]' | head -n 1)
+	fi
+	local lsbrelease=$(lsb_release -sc 2>/dev/null || echo "")
+	if [ "${lsbrelease}" != "" -a "${lsbrelease}" = "trusty" ]; then
+		echo "starting mongod service directly with init script..."
 		/etc/init.d/mongod start
-	else 
+	elif [ "${redhatrelease}" = "5"  ]; then
+		echo "starting mongod service directly with init script..."
+		/etc/init.d/mongod start
+	else
         	echo "starting mongod service... "
 		service mongod start
 	fi
@@ -18,10 +30,18 @@ function start_service {
 }
 
 function stop_service {
-	if [ "$(lsb_release -sc)" = "trusty" ]; then
-        	echo "stopping mongod service on trusty..."
+        local redhatrelease=""
+        if [ -f /etc/redhat-release ]; then
+		redhatrelease=$(cat /etc/redhat-release | grep -o '[0-9]' | head -n 1)
+	fi
+	local lsbrelease=$(lsb_release -sc 2>/dev/null || echo "")
+	if [ "${lsbrelease}" != "" -a "${lsbrelease}" = "trusty" ]; then
+		echo "stopping mongod service directly with init script..."
 		/etc/init.d/mongod stop
-	else 
+	elif [ "${redhatrelease}" = "5"  ]; then
+		echo "stopping mongod service directly with init script..."
+		/etc/init.d/mongod stop
+	else
         	echo "stopping mongod service... "
 		service mongod stop
 	fi
@@ -49,7 +69,19 @@ function clean_datadir {
 	fi
 }
 
-for engine in mmapv1 PerconaFT rocksdb wiredTiger inMemory; do
+function test_hotbackup {
+	rm -rf /tmp/backup
+	mkdir -p /tmp/backup
+	chown mongod:mongod -R /tmp/backup
+	BACKUP_RET=$(mongo admin --eval 'db.runCommand({createBackup: 1, backupDir: "/tmp/backup"})'|grep -c '"ok" : 1')
+	rm -rf /tmp/backup
+	if [ ${BACKUP_RET} = 0 ]; then
+		echo "Backup failed for storage engine: ${engine}"
+		exit 1
+	fi
+}
+
+for engine in mmapv1 PerconaFT rocksdb wiredTiger; do
 	stop_service
 	clean_datadir
 	sed -i "/engine: *${engine}/s/#//g" /etc/mongod.conf
@@ -58,6 +90,10 @@ for engine in mmapv1 PerconaFT rocksdb wiredTiger inMemory; do
 	echo "importing the sample data"
 	mongo < /vagrant/mongo_insert.js >> $log
 	list_data >> $log
+	echo "testing the hotbackup functionality"
+	if [ ${engine} = "wiredTiger" -o ${engine} = "rocksdb" ]; then
+		test_hotbackup
+	fi
 	stop_service
 	echo "disable ${engine}"
 	sed -i "/engine: *${engine}/s//#engine: ${engine}/g" /etc/mongod.conf
