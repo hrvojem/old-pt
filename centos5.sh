561005f55a40332b559d1df74e4cff6ff50b77f1
hrvojem <hmatijak@gmail.com>
2017-01-10 22:13:37 +0100

    PS 5.5 version bump

--- centos5.sh
+++ centos5.sh
@@ -1,6 +1,6 @@
 yum install -y python-simplejson.x86_64 vim-enhanced
-wget https://dl.fedoraproject.org/pub/epel/5/x86_64/epel-release-5-4.noarch.rpm
-#wget anorien.csc.warwick.ac.uk/mirrors/epel/5/x86_64/epel-release-5-4.noarch.rpm
+#wget https://dl.fedoraproject.org/pub/epel/5/x86_64/epel-release-5-4.noarch.rpm
+wget https://anorien.csc.warwick.ac.uk/mirrors/epel/5/x86_64/epel-release-5-4.noarch.rpm
 rpm -ivH epel-release-5-4.noarch.rpm
-rpm -ivH /vagrant/percona-release-0.1-3.noarch.rpm
+rpm -ivH /vagrant/percona-release-0.1-4.noarch.rpm
 yum install -y Percona-Server-shared-compat.x86_64 
