635e742424bb9735e491cb41ad9b2c1d58083ac0
hrvojem <hmatijak@gmail.com>
2018-07-13 14:40:10 +0200

    Improved ps output and update package version for 3.6 playbook

--- ps.sh
+++ ps.sh
@@ -1,1 +1,1 @@
-ps auxww | grep mysql
+echo "===== MYSQL ====="; ps aux|grep "mysqld "|grep -v grep; echo -e "\n===== MONGODB ====="; ps aux|grep "mongod "|grep -v grep; echo -e "\n===== VAULT ====="; ps aux|grep "vault "|grep -v grep
